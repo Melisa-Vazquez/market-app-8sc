1f2fd49ebd3ca9099bdf41b5a6f05fd8e52e2c23
Your Name <you@example.com>
2025-09-02 22:21:21 -0600

    HelloWordControler added

--- market-app-8sc/src/test/java/com/itmerida/market_app_8sc/MarketApp8scApplicationTests.java
+++ market-app-8sc/src/test/java/com/itmerida/market_app_8sc/MarketApp8scApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MarketApp8scApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
